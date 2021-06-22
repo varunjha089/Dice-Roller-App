09e3cdd3fd1a247483101081840657ae812c949a
Varun Kumar <varunjha089@gmail.com>
2021-06-22 17:04:07 +0530

    Dice logic implemented in the code.

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -14,12 +14,28 @@ class MainActivity : AppCompatActivity() {
         // Finding the button in layout and saving the reference in variable 'rollButton'
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
+            // Displaying a toast message in starting.
             val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
             toast.show()
 
             // Updating the textView
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "6"
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        // TODO("Not yet implemented")
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSide: Int){
+
+    // A roll() method to generate a random number between 1 and 6.
+    fun roll(): Int{
+        return (1..numSide).random()
     }
 }
